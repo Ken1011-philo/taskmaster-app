231557fdb3b5ed5cf43e82c3d63e34a6ff22500e
shooooo1025 <tennis.sho.s@gmail.com>
2025-11-06 14:58:25 +0900

    ナビゲーションバーの作成、それに伴いNavigationBarApp.tsxの追加 main.tsxをmain.tsxで読み込むようにした。

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
@@ -7,6 +6,7 @@ import {
   createBrowserRouter,
   Navigate,
 } from "react-router-dom";
+import Layout from "./components/Layout";
 
 import Plan from "./pages/Plan";
 import Do from "./pages/Do";
@@ -15,12 +15,23 @@ import Setting from "./pages/setting";
 import Login from "./pages/login";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Navigate to="/Plan" /> },
-  { path: "/Plan", element: <Plan /> },
-  { path: "/Do", element: <Do /> },
-  { path: "/Focus", element: <Focus /> },
-  { path: "/Setting", element: <Setting /> },
-  { path: "/Login", element: <Login /> },
+  {
+    path: "/",
+    element: <Layout />, 
+    children: [
+      { path: "/Plan", element: <Plan /> },
+      { path: "/Do", element: <Do /> },
+      { path: "/Setting", element: <Setting /> },
+    ],
+  },
+  {
+    path: "/Focus",
+    element: <Focus />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
